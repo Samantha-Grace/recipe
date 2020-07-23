5c1463af9ef65172f4a63a7d7ff6d073c216a86d
Samantha Gagen <sgagen@r1rcm.com>
2020-07-23 13:10:01 -0600

    Updates

---
+++ precache-manifest.522c803ff6edcd8dc964f70168b832ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b86a458d9ff370e30f66da83956f736f",
+    "url": "/reactrecipe/index.html"
+  },
+  {
+    "revision": "4219fd17878e936b8fc1",
+    "url": "/reactrecipe/static/css/main.e68ec4e2.chunk.css"
+  },
+  {
+    "revision": "1d0d8331dc39718f2a7c",
+    "url": "/reactrecipe/static/js/2.27ff0690.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactrecipe/static/js/2.27ff0690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4219fd17878e936b8fc1",
+    "url": "/reactrecipe/static/js/main.1054a012.chunk.js"
+  },
+  {
+    "revision": "2d1aae4f26658c5287c7",
+    "url": "/reactrecipe/static/js/runtime-main.b14488bc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactrecipe/static/media/logo.5d5d9eef.svg"
+  }
+]);
